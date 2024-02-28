829eb6f06b5e05a0afa0d7a951a8cd53756766df
Anastasiia Ünal <velyka.nastya@icloud.com>
2024-02-28 22:54:04 +0100

    add delete signal

--- video/signals.py
+++ video/signals.py
@@ -1,4 +1,5 @@
-from django.db.models.signals import post_save
+import os
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from .models import Video
 
@@ -6,4 +7,16 @@ from .models import Video
 def video_post_save(sender, instance, created, **kwargs):
     print('Video wurde gespreichert')
     if created:
-        print('New Video created')       
+        print('New Video created')
+        
+
+
+@receiver(post_delete, sender=Video)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    """
+    Deletes file from filesystem
+    when corresponding `Video` object is deleted.
+    """
+    if instance.video_file:
+        if os.path.isfile(instance.video_file.path):
+            os.remove(instance.video_file.path)
