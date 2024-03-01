21a75a35c83b07877b7dfc8b42556daa1e2dca1b
Anastasiia Ünal <velyka.nastya@icloud.com>
2024-03-01 01:58:24 +0100

    convert video in different quality, delete all videos

--- video/signals.py
+++ video/signals.py
@@ -1,7 +1,7 @@
 import os
 from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
-from video.tasks import convert_video_480p
+from video.tasks import convert_video_360p, convert_video_720p, convert_video__1080p
 from .models import Video
 
 
@@ -10,11 +10,11 @@ def video_post_save(sender, instance, created, **kwargs):
     """
     Save file to filesystem
     """
-    print('Video wurde gespreichert')
     if created:
-        convert_video_480p(instance.video_file.path)
+        convert_video_360p(instance.video_file.path)
+        convert_video_720p(instance.video_file.path)
+        convert_video__1080p(instance.video_file.path)
         
-
 
 @receiver(post_delete, sender=Video)
 def auto_delete_file_on_delete(sender, instance, **kwargs):
@@ -22,6 +22,13 @@ def auto_delete_file_on_delete(sender, instance, **kwargs):
     Deletes file from filesystem
     when corresponding `Video` object is deleted.
     """
+    converts = ['_360p', '_720p', '_1080p']
+    
     if instance.video_file:
         if os.path.isfile(instance.video_file.path):
             os.remove(instance.video_file.path)
+            for p in converts:
+                filename, extension = os.path.splitext(instance.video_file.path)
+                converted_video = filename + p + extension
+                if os.path.isfile(converted_video):
+                    os.remove(converted_video)
--- video/tasks.py
+++ video/tasks.py
@@ -2,11 +2,31 @@ import os
 import subprocess
 
 
-def convert_video_480p(input):
+def convert_video_360p(input):
     """
-    Convert Video in 480p with ffmpeg and save it to filesystem
+    Convert Video from Input in 360p with ffmpeg and save it to filesystem
     """
     filename, extension = os.path.splitext(input)
-    output = filename + '_480p' + extension
-    cmd = 'ffmpeg ' + '-i ' + input + ' -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 ' + output
+    output = filename + '_360p' + extension
+    cmd = 'ffmpeg ' + '-i ' + input + ' -vf scale=-1:360 -c:v libx264 -crf 18 -c:a aac -strict -2 ' + output
+    subprocess.run(cmd, capture_output=True)
+    
+
+def convert_video_720p(input):
+    """
+    Convert Video from Input in 720p with ffmpeg and save it to filesystem
+    """
+    filename, extension = os.path.splitext(input)
+    output = filename + '_720p' + extension
+    cmd = 'ffmpeg ' + '-i ' + input + ' -s hd720 -c:v libx264 -crf 23 -c:a aac -strict -2 ' + output
+    subprocess.run(cmd, capture_output=True)
+    
+
+def convert_video__1080p(input):
+    """
+    Convert Video from Input in 10800p with ffmpeg and save it to filesystem
+    """
+    filename, extension = os.path.splitext(input)
+    output = filename + '_1080p' + extension
+    cmd = 'ffmpeg ' + '-i ' + input + ' -s hd1080 -c:v libx264 -crf 23 -c:a aac -strict -2 ' + output
     subprocess.run(cmd, capture_output=True)
